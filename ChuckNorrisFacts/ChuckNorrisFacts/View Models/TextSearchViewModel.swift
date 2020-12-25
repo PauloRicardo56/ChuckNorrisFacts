254b94f06505628c23a1ecddb60743f14cb2463e
Paulo Ricardo <praraujo.vieira@gmail.com>
2020-12-25 17:13:26 -0300

    create: retries when no internet connection error

--- ChuckNorrisFacts/ChuckNorrisFacts/View Models/TextSearchViewModel.swift
+++ ChuckNorrisFacts/ChuckNorrisFacts/View Models/TextSearchViewModel.swift
@@ -11,9 +11,23 @@ import RxCocoa
 
 class TextSearchViewModel: ViewModel {
     
+    let maxErrorAttempts = 3
+    
     func searchFacts(_ searchText: String) -> Observable<[Fact]> {
         ChuckNorrisAPI.shared.searchFact(searchText)
+            .retry { err in
+                err.enumerated()
+                    .flatMap { [weak self] attempt, err -> Observable<Int> in
+                        guard let self = self else { return .error(err) }
+                        if attempt >= self.maxErrorAttempts {
+                            return .error(err)
+                        }
+                        return .timer(.seconds(3), scheduler: MainScheduler.instance)
+                    }
+            }
             .map(\.result)
     }
+    
+    // MARK: Error handling
     
 }
