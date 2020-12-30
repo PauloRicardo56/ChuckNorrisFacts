fc395f1db07d98992595de5a7000d215e04ad678
Paulo Ricardo <praraujo.vieira@gmail.com>
2020-12-29 23:50:08 -0300

    create: configured fastlane tests

--- ChuckNorrisFacts/ChuckNorrisFactsTests/TextSearchViewModelTests.swift
+++ ChuckNorrisFacts/ChuckNorrisFactsTests/TextSearchViewModelTests.swift
@@ -7,24 +7,19 @@
 
 import Foundation
 import XCTest
-import RxSwift
 import RxBlocking
 @testable import ChuckNorrisFacts
 
 class TextSearchViewModelTests: XCTestCase {
     
-    /// Como estou usando o block em um relay (nunca termina) foi
-    /// necessário skipar o primeiro valor (default do init) e pegar o
-    /// primeiro seguinte (resultado da requisição).
     func test_searchedFacts() {
         let sut = TextSearchViewModel()
         
         sut.searchFact("cook")
         let searchedResult = try! sut.facts
-            .skip(1)
             .toBlocking()
             .first()!
-        
+
         XCTAssertTrue(searchedResult.count > 0)
     }
 }
